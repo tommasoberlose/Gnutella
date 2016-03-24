a425b052e9a466106c1278c51719e1032651e142
Tommaso Berlose <tommaso.berlose@gmail.com>
2016-03-24 17:12:30 +0100

    Moved search

--- Function.py
+++ Function.py
@@ -2,6 +2,11 @@ import string
 import random
 import socket
 import shutil
+import os
+import sys
+import hashlib
+import Constant as const
+import Function as func
 
 ####### STRINGHE
 
@@ -86,3 +91,24 @@ def get_ipv4(ip):
 
 def get_ipv6(ip):
 	return ip[16:55]
+
+
+###### SEARCH FILE
+
+#funzione di ricerca file all'interno della cartella FileCondivisi
+def search_file(query):
+	file_list = []
+	file_found_list = []
+	file_list = os.listdir(const.FILE_COND)
+	for file in file_list:
+		if query in file:
+			if not file.endswith('~'):
+				md5File = hashlib.md5(open(const.FILE_COND + file,'rb').read()).hexdigest()
+				file_found = [md5File, file]
+				file_found_list.append(file_found)
+		else:
+			######gestire gli errori
+			func.error("File not exists")
+
+	print(file_found_list)
+	return file_found_list
